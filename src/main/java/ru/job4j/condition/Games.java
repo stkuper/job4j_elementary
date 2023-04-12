189446f1dd88b18e7b2250c9921a8ab297ce7cf0
stkuper <stklipper@gmail.com>
2023-04-12 18:32:30 +0300

    4.3.1. Сокращенный оператор && [#333783]

---
+++ src/main/java/ru/job4j/condition/Games.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+public class Games {
+    public static void permission(boolean allowByParrent, boolean hasMoney) {
+        if (allowByParrent && hasMoney) {
+            System.out.println("I can go to the computer club.");
+        } else {
+            System.out.println("I can't.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Games.permission(true, true);
+        Games.permission(true, false);
+        Games.permission(false, true);
+        Games.permission(false, false);
+    }
+}
