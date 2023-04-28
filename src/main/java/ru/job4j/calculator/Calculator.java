2ac5b07c7c633ab6b0da450e6bf2d2c2c240efe4
stkuper <stklipper@gmail.com>
2023-04-28 15:38:55 +0300

    Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -13,21 +13,6 @@ public class Calculator {
     }
 
     public static void main(String[] args) {
-        /*
-        int one = 1;
-        int two = 2;
-        int six = 6;
-        int four = 4;
-        int five = 5;
-        int onePlusTwo = one + two;
-        int sixDivTwo = six / two;
-        int fiveMinusTwo = five - two;
-        int fourTimeTwo = four * two;
-        System.out.println(onePlusTwo);
-        System.out.println(sixDivTwo);
-        System.out.println(fiveMinusTwo);
-        System.out.println(fourTimeTwo);
-        */
         plus(1, 2);
         plus(10, 11);
         minus(7, 3);
